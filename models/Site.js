29abbb6db0d2e974b44c2d47920f3bde86393b57
Micah Kramer <beta.grumm@gmail.com>
2018-04-05 11:46:23 -0700

    Rearranged files. Added nodemon for hot reloading. Added Mocha as dev dep.

--- models/Site.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const SiteSchema = new mongoose.Schema({
-    subdomain: String,
-    anohterField: String
-})
-
-module.exports = mongoose.model('Site', SiteSchema);
