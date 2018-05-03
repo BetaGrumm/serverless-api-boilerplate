00d0c758c2f233efc99d10d1e12e24142f75fc5f
Micah Kramer <beta.grumm@gmail.com>
2018-05-03 16:32:29 -0700

    Worked on the ghost node process issue. Tweaked a few things, but it's boiling down to a possible nodemon issue, or dep of some kind.

--- utils/db.js
+++ utils/db.js
@@ -4,16 +4,16 @@ const path = require('path');
 require('dotenv').config({path: path.resolve(process.cwd(), 'vars.env')});
 let isConnected;
 
-module.exports = connectToDatabase = () => {
-    if (isConnected) {
-        console.log('=> using existing database connection...');
-        return Promise.resolve();
-    }
-
-    console.log('=> creating new database connection...');
-    console.log('Connection String: ' + process.env.DB);
-    return mongoose.connect(process.env.DB)
-        .then(db => {
-            isConnected = db.connections[0].readyState;
-        });
-};
+// module.exports = connectToDatabase = () => {
+//     if (isConnected) {
+//         console.log('=> using existing database connection...');
+//         return Promise.resolve();
+//     }
+//     console.log('=> creating new database connection...');
+//     let uri = process.env.DB_URI
+//     console.log("URI " + uri);
+//     return mongoose.connect(uri)
+//         .then(db => {
+//             isConnected = db.connections[0].readyState;
+//         });
+// };
